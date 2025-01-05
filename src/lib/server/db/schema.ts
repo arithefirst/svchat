fe14426f852d2497b477399fb6f709f8d7dcb423
April Hall <ari@arithefirst.com>
2025-01-05 16:06:57 -0500

    feat: Add schema for messages

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,9 +1,18 @@
-import { pgTable, uuid, text } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+import { sql } from 'drizzle-orm';
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey(),
   username: text('username'),
-  displayname: text('display_name'),
+  displayName: text('display_name'),
   salt: text('salt'),
   hash: text('hash'),
+});
+
+export const messages = pgTable('messages', {
+  id: uuid('id').primaryKey(),
+  user_id: uuid('user_id').references(() => users.id),
+  channel_id: uuid('channel_id'),
+  message: text('message'),
+  timestamp: timestamp('timestamp', { mode: 'date' }).default(sql`now()`),
 });
