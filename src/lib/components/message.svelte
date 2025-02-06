7a0aa70e0e1e2ed67d00866200454283042c56bd
April Hall <ari@arithefirst.com>
2025-02-05 19:18:05 -0500

    fix: Change gray to zinc

--- src/lib/components/message.svelte
+++ src/lib/components/message.svelte
@@ -3,7 +3,7 @@
   const { message, imageSrc, user }: TypeMessage = $props();
 </script>
 
-<div class="w-full hover:bg-gray-200 flex py-2 dark:hover:bg-stone-900">
+<div class="w-full hover:bg-zinc-200 flex py-2 dark:hover:bg-stone-900">
   <div class="avatar mx-2 rounded-sm">
     <div class="w-12 h-12 overflow-hidden rounded-lg border bg-white">
       <img src={imageSrc} alt="Profile image for {user}" />
