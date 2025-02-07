41a1a646778935602ed12d09dbc61eb06be9670e
April Hall <ari@arithefirst.com>
2025-02-06 23:57:24 -0500

    fix: Remove $inspect (left over from testing)

--- src/lib/components/channel.svelte
+++ src/lib/components/channel.svelte
@@ -8,8 +8,6 @@
 
   const { channelName }: Props = $props();
   const color = $derived(page.params.channel === channelName ? 'text-primary' : 'text-muted-foreground hover:text-primary');
-
-  $inspect(page.params.channel, color);
 </script>
 
 <a href={`/channel/${channelName}`} class={`flex items-center gap-3 rounded-lg px-3 py-2 transition-all ${color}`}>
