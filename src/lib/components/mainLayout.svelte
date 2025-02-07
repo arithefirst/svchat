e71b524d15238ffc0d340c7f2f0fc7e8dfeb70ca
April Hall <ari@arithefirst.com>
2025-02-07 13:48:03 -0500

    feat: Collapsable sidebar on mobile

--- src/lib/components/mainLayout.svelte
+++ src/lib/components/mainLayout.svelte
@@ -20,11 +20,14 @@
     children: Snippet;
   }
 
+  let open: boolean = $state(false);
+  let mobileSidebarTransform: string = $derived(open ? '' : '@apply -translate-x-[100%] md:translate-x-0');
+
   const { data, channels, children }: Props = $props();
 </script>
 
 <div class="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-  <div class="bg-muted/40 hidden border-r md:block">
+  <div class={'bg-muted/40 border-r md:w-[220px] lg:w-[280px] w-3/4 h-full md:relative absolute ' + mobileSidebarTransform}>
     <div class="flex h-full max-h-screen flex-col gap-2">
       <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
         <a href="/" class="flex items-center gap-2 font-semibold">
