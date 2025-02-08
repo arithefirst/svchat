9b8baefc360c94229e92fc0b42dd97e808d267af
April Hall <ari@arithefirst.com>
2025-02-07 22:32:34 -0500

    fix: Prevent textarea from being user resizeable

--- src/routes/channel/[channel]/+page.svelte
+++ src/routes/channel/[channel]/+page.svelte
@@ -66,9 +66,9 @@
       bind:value={msg}
       bind:this={textareaRef}
       use:autoResize
-      class="flex h-10 min-h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm
-      placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring
-       disabled:cursor-not-allowed disabled:opacity-50"></textarea>
+      class="flex h-10 min-h-10 w-full resize-none rounded-md border border-input bg-transparent px-3 py-2 text-sm
+      shadow-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1
+       focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"></textarea>
     <Button class="h-full min-h-10 w-14" type="submit"><Send class="size-full" /></Button>
     <button type="submit" bind:this={submitRef} class="hidden">submit</button>
   </form>
