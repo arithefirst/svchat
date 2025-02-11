4149066d745f9c7718650c93ee8a5d55a83df929
April Hall <ari@arithefirst.com>
2025-02-11 12:09:09 -0500

    fix: Allow codeblocks to expand to fit content
    Note: Codeblocks can only be come up to 95% width of their container,
    after that the `<pre>` will start scrolling.

--- src/lib/components/prose.svelte
+++ src/lib/components/prose.svelte
@@ -9,4 +9,6 @@
   const { children, class: className = '' }: Props = $props();
 </script>
 
-<div class={className + ' prose prose-stone prose-blue dark:prose-invert'}>{@render children()}</div>
+<div class={className + ' prose prose-stone prose-blue !max-w-full dark:prose-invert prose-pre:w-fit prose-pre:max-w-[95%] '}>
+  {@render children()}
+</div>
