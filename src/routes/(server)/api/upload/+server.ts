724e4737558ba1d1955e794f3d0990f4be04e819
April Hall <ari@arithefirst.com>
2025-02-21 18:37:54 -0500

    fix: Prevent unauthed users from uploading

--- src/routes/(server)/api/upload/+server.ts
+++ src/routes/(server)/api/upload/+server.ts
@@ -1,3 +1,14 @@
-export const POST = async () => {
+import { error } from '@sveltejs/kit';
+import { auth } from '$lib/server/db/auth';
+
+export const POST = async ({ request }) => {
+  const session = await auth.api.getSession({
+    headers: request.headers,
+  });
+
+  if (!session) {
+    return error(401, 'Not authorized. Please sign up at /sign-up');
+  }
+
   return new Response(undefined, { status: 204 });
 };
