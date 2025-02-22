9711b0c92627a9b6308d087177c9d6ba5e83f093
April Hall <ari@arithefirst.com>
2025-02-22 02:44:39 -0500

    fix: Server sometimes throwing 308 and ignoring requests

--- src/lib/functions/generateReadableStream.ts
+++ src/lib/functions/generateReadableStream.ts
@@ -2,7 +2,7 @@ export async function generateStream(file: File): Promise<Response> {
   const formData = new FormData();
   formData.append('file', file);
 
-  const res = await fetch(`/api/set-profile-photo/`, {
+  const res = await fetch(`/api/set-profile-photo`, {
     method: 'POST',
     body: formData,
   });
