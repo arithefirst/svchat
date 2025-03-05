193bd380c3a95932f60f53b7d0e464a70930578b
April Hall <ari@arithefirst.com>
2025-03-05 08:30:49 -0500

    fix: Make profile image clickable to upload new one

--- src/lib/components/forms/updatePFP.svelte
+++ src/lib/components/forms/updatePFP.svelte
@@ -30,8 +30,8 @@
       }}
     >
       <div class="relative">
-        <ImageCropper.Preview class="rounded-md" />
         <ImageCropper.UploadTrigger>
+          <ImageCropper.Preview class="rounded-md bg-white" />
           <div class="absolute -bottom-3 -left-3 size-9 rounded-lg {buttonVariants({ variant: 'outline' })}">
             <Edit class="size-4" />
           </div>
